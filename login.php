4662c56bf77e48ff01db4d3cf6cc15be5a8508a7
laurencerogador-ops <laurencerogador@gmail.com>
2026-04-28 04:12:22 -0700

    Update login.php

--- login.php
+++ login.php
@@ -24,7 +24,7 @@ if(isset($_POST['login'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GatePass | Login</title>
+    <title>GatePass | user</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body { background: #f0f2f5; height: 100vh; display: flex; align-items: center; justify-content: center; font-family: 'Segoe UI', sans-serif; }
